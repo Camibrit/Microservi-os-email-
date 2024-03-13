d53bc61d8c75a4ee1002a8016ba6ba20757dd6c8
Camibrit <camila-brito10@hotmail.com>
2024-03-13 14:24:31 -0300

    the other classes

---
+++ src/main/java/microservice/com/microservices/repositories/EmailRepository.java
@@ -0,0 +1,9 @@
+package microservice.com.microservices.repositories;
+
+import microservice.com.microservices.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
